c08f64cfc2c70da81c3359e8217c8053e72fd478
ninhthuanntnt <53461693+ninhthuanntnt@users.noreply.github.com>
2020-11-14 22:57:21 +0700

    YourGallery - Release V1.0

--- src/main/java/models/dao/util/DBUtil.java
+++ src/main/java/models/dao/util/DBUtil.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 
 public class DBUtil {
     private static String dbURL = "jdbc:mysql://localhost:3306/your_gallery_db?characterEncoding=UTF-8";
-    private static String dbUsername = "root";
-    private static String dbPassword = "Thuanthuc123";
+    private static String dbUsername = System.getEnv("dbUsername");
+    private static String dbPassword = System.getEnv("dbPassword");
     public static Connection getConnection(){
         Connection conn = null;
         try {
